c24ed971036f7c711070311d11791678a7c54352
szajberpiraty <szajberpiraty@gmail.com>
2018-09-30 11:20:22 +0200

    Using és anélküli használat

--- dispose_pelda1/Program.cs
+++ dispose_pelda1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,18 @@ namespace dispose_pelda1
     {
         static void Main(string[] args)
         {
+            int i=0;
+
+            while (!Console.KeyAvailable)
+            {
+                //Console.WriteLine(i);
+                //var stream = new MemoryStream(100000); //Folyamatosan megy a szemétgyűjtő
+                var bitmap = new Bitmap(1280, 1024);
+                //Van véglegesítője, ezért nagyon könnyen elfogy a memória
+                //using( var bitmap = new Bitmap(1280,1024)) {} //-Így nem lesz memória probléma
+                Console.WriteLine(GC.GetTotalMemory(false));
+                i++;
+            }
         }
     }
 }
