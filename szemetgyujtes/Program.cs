82a3caa43bc90560c22054587bf761d731934962
szajberpiraty <szajberpiraty@gmail.com>
2018-09-30 10:10:43 +0200

    A destruktor működése, automatikus szemétgyűjtő

--- szemetgyujtes/Program.cs
+++ szemetgyujtes/Program.cs
@@ -10,6 +10,29 @@ namespace szemetgyujtes
     {
         static void Main(string[] args)
         {
+            //Szemétgyűjtés: a .NET automatikusan felszabadítja azon objektumok helyét, amelyekre már nem mutat referencia
+            //Az osztályokban definiált destruktorok is ilyenkor futnak le.
+            //A destruktor/finalizer lefutása automatikus.
+
+            for (int i = 0; i < 100; i++)
+            {
+                var dest = new Destroyable();
+                //GC.Collect(); //kézzel híjuk a szemétgyűjtő algoritmust-nem feltétlenül javasolt megoldás
+
+            }
+            Console.ReadLine();
+
+        }
+        class Destroyable
+        {
+            public Destroyable()
+            {
+                Console.WriteLine("Konstruktor");
+            }
+            ~Destroyable()
+            {
+                Console.WriteLine("Finalizer");
+            }
         }
     }
 }
